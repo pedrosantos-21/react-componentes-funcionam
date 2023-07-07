f183b8e2943143a0ad336d37cd99d73cc1c01f27
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-07-07 14:58:49 -0300

    Finalizando o projeto

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,17 +1,22 @@
 import Colaborador from '../Colaborador'
 import './time.css'
+import hexToRgba from 'hex-to-rgba';
 
-const Time = ({ time, colaboradores }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
-
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+        colaboradores.length > 0 && <section className='time' style={{  backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
+            <input onChange={evento => mudarCor(evento.target.value, time.id)} value={time.cor} type='color' className='input-cor' />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+            <div className='deletar' onClick={aoDeletar}></div>
             <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
+                {colaboradores.map((colaborador, indice) => { return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar}/>} )}
             </div>
         </section>
-
     )
 }
 
 export default Time
+
+/* É necessário a utilização do onChange */
+//target porque vai ser utilizado por alguém.
+//Primeiro precisamos passar a cor para o input e depois o "nome"
